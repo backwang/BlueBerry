caba7f285992d15efd487867ee2f605958989054
backwang <backwang@qq.com>
2013-01-16 11:20:25 +0800

    启动界面的动画切换

--- blueberry/src/com/example/blueberry/LoginActivity.java
+++ blueberry/src/com/example/blueberry/LoginActivity.java
@@ -1,8 +1,12 @@
 package com.example.blueberry;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
 
 public class LoginActivity extends Activity {
 
@@ -10,6 +14,32 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_launch);
+		
+		final Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+		RelativeLayout myLayout = (RelativeLayout) findViewById(R.id.launchlayout);
+		Animation animation = AnimationUtils.loadAnimation(this,R.anim.launch_exit);  
+		animation.setAnimationListener(new AnimationListener() {
+			
+			@Override
+			public void onAnimationStart(Animation animation) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				// TODO Auto-generated method stub
+				 startActivity(intent);
+			}
+		});
+		 myLayout.startAnimation(animation);
+		
 	}
 
 	 

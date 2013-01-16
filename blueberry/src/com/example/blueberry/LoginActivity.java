19c4d6f94be689a1649ee9357b5454baafe9653c
backwang <backwang@qq.com>
2013-01-16 10:27:15 +0800

    添加了启动界面的背景图片

--- blueberry/src/com/example/blueberry/LoginActivity.java
+++ blueberry/src/com/example/blueberry/LoginActivity.java
@@ -9,14 +9,9 @@ public class LoginActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+		setContentView(R.layout.activity_launch);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_login, menu);
-		return true;
-	}
+	 
 
 }

b98393949bfe6b6012112739b48f423f9b1aabe7
backwang <backwang@qq.com>
2013-01-17 17:02:05 +0800

    1-17 17:01代码修正

---
+++ blueberry/src/com/example/dataadapter/ListViewDataAdapter.java
@@ -0,0 +1,35 @@
+package com.example.dataadapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class ListViewDataAdapter  
+{
+    BaseAdapter baseAdapter=new BaseAdapter() {
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+	};
+}

a29e093d9bf97621d869b7652127c7d08abd1cdc
backwang <backwang@qq.com>
2013-01-28 16:56:52 +0800

    子VIEW中的按钮事件测试

--- blueberry/src/com/bk/blueberry/MoreActivity.java
+++ blueberry/src/com/bk/blueberry/MoreActivity.java
@@ -15,34 +15,41 @@ public class MoreActivity extends MainActivity implements OnClickListener {
 
 	Button btn_login;
 	RelativeLayout rel_top_bar;
-    ImageButton btn_return;
-	 
+	ImageButton btn_return;
+    LoginActivity loginActivity;
 	public void InitView(RelativeLayout rel_top_bar) {
 		// TODO Auto-generated method stub
-		 
-		 
+
 		btn_login = (Button) (rel_top_bar.findViewById(R.id.btn_login));
 		btn_login.setOnClickListener(this);
-		 
+
 		btn_return = (ImageButton) (rel_top_bar.findViewById(R.id.btn_return));
 		btn_return.setOnClickListener(this);
 		this.rel_top_bar = rel_top_bar;
-		
+
 	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		LayoutParams layoutParams = new LayoutParams();
 		switch (v.getId()) {
 		case R.id.btn_login:
+
+			
+			View layout_activity_login = MainActivity.inflater.inflate(
+					R.layout.activity_login, null);
+			
+			layoutParams.gravity = Gravity.CENTER;
+			rel_body_content.addView(layout_activity_login, layoutParams);
 			 
-			View layout_activity_login = MainActivity.inflater.inflate(R.layout.activity_login,null);
-			LayoutParams layoutParams=new LayoutParams();
-			layoutParams.gravity=Gravity.CENTER;
-			rel_body_content.addView(layout_activity_login,layoutParams);
+			LoginActivity loginActivity =new LoginActivity();
+			loginActivity.InitView(rel_body_content);
 			break;
 		case R.id.btn_return:
-			View layout_activity_main = MainActivity.inflater.inflate(R.layout.activity_main,null);
+			View layout_activity_main = MainActivity.inflater.inflate(
+					R.layout.activity_main, null);
+			
 			rel_body_content.addView(layout_activity_main);
 			break;
 		default:
@@ -50,5 +57,4 @@ public class MoreActivity extends MainActivity implements OnClickListener {
 		}
 	}
 
-	 
 }
